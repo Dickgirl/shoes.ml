18a6aaac1041135d40e9cefe1f515c549e9d791e
Dickgirl <Kaleb173@gmail.com>
2022-03-06 06:22:41 +0000

    Add files via upload

---
+++ database.js
@@ -0,0 +1,102 @@
+new Vue({
+  el: '#app',
+  data: {
+jordan4: [
+  {
+    name: 'Jordan 4 Retro Black Cat (2020)',
+    image: './images/Jordan 4 Retro Black Cat.png',
+    price: '500.00',
+    id: '621c4595ba110',
+    minsize: '6',
+    maxsize: '12',
+    colour: 'product--black'
+  },
+  {
+    name: 'Jordan 4 Retro University Blue',
+    image: './images/Jordan 4 Retro Black Cat.png',
+    price: '350.00',
+    id: '621c45e3a9602',
+    minsize: '7',
+    maxsize: '13',
+    colour: 'product--blue'
+  }
+],
+jordan1: [
+  {
+    name: 'Jordan 1 Retro High Bred Toe',
+    image: './images/Jordan 1 Retro High Bred Toe.png',
+    price: '500.00',
+    id: '621c45e686cea',
+    minsize: '6',
+    maxsize: '11',
+    colour: 'product--red'
+  },
+  {
+    name: 'Jordan 1 Retro Chicago (2015)',
+    image: './images/Jordan 1 Retro Chicago.png',
+    price: '700.00',
+    id: '621c45eb7d9d4',
+    minsize: '6.5',
+    maxsize: '11',
+    colour: 'product--red'
+  },
+  {
+    name: 'Jordan 1 Retro High Off-White Chicago',
+    image: './images/Jordan 1 Retro High Off-White Chicago.png',
+    price: '1200.00',
+    id: '621c45e686cea',
+    minsize: '7',
+    maxsize: '12',
+    colour: 'product--red'
+  },
+  {
+    name: 'Jordan 1 Retro High Travis Scott',
+    image: './images/Jordan 1 Retro Hights.png',
+    price: '700.00',
+    id: '621c45ea21633',
+    minsize: '6.5',
+    maxsize: '12',
+    colour: 'product--brown'
+  },
+  {
+    name: 'Jordan 1 Retro High Off-White University Blue',
+    image: './images/Jordan 1 ubs.png',
+    price: '700.00',
+    id: '621c45ea21633',
+    minsize: '3',
+    maxsize: '12',
+    colour: 'product--blue'
+  },
+  {
+    name: 'Jordan 1 Retro High Off-White White',
+    image: './images/Jordan 1 Retro High Off-White White.png',
+    price: '1200.00',
+    id: '621c4908cbd08',
+    minsize: '7',
+    maxsize: '13',
+    colour: 'product--white'
+  }
+],
+featuredshoe: [
+  {
+    name: 'Jordan 4 Retro Black Cat (2020)',
+    image: './images/Jordan 4 Retro Black Cat.png',
+    price: '500.00',
+    id: '621c4595ba110',
+    minsize: '6',
+    maxsize: '12',
+    colour: 'product--black'
+  },
+  {
+    name: 'Jordan 1 Retro High Off-White White',
+    image: './images/Jordan 1 Retro High Off-White White.png',
+    price: '1200.00',
+    id: '621c4908cbd08',
+    minsize: '7',
+    maxsize: '13',
+    colour: 'product--white'
+  }
+],
+
+
+}})
